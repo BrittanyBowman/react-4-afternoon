c4af99d866c57c8b608cd5c4cf6ae9f56a230ec9
Brittany Bowman <britpetty@gmail.com>
2019-02-16 11:18:10 -0700

    completed assignment

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
 
 export default class ClassList extends Component {
   constructor() {
-    super()
-    
+    super();
+
+    this.state = {
+      students: []
+    };
   }
 
-  render() {
-    return (
-      <div className="box">
-        <h1></h1>
-        <h2>ClassList:</h2>
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students?class=${ this.props.match.params.class }`).then( results => {
+      this.setState({
+        students: results.data
+      });
+    });
+  }
 
+  // BELOW: we are using map to render each student's first and last name in an <h3> element. React requires a unique KEY, I've always used "id" before, but here we are using the "index of the map", which I do not understand. What is the "index of the map"? See also { students } under the <h2>ClassList:, where we are RENDERING the <h3> element information of first and last name of each DIFFERENT student from the json API.
+  render() {
+    const students = this.state.students.map((student, i) => (
+      <Link to={`/student/${student.id}`} key={ i }>
+        <h3>{ student.first_name } { student.last_name }</h3>
+      </Link>
+    ));
+    /*update the h1 element to display the current class. Just like how we accessed the route parameter for our axios request, we can do the same thing here IN the h1 using { } in our JSX.*/
+    return (
+      <div className='box'>
+        <h1>{ this.props.match.params.class }</h1>
+        <h2>ClassList:</h2>
+        { students }
       </div>
     )
   }
